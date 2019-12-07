ef79b732cb9b73913a23d490fa430a96148391da
Gabriel Soares <gssoares@arezzo.com.br>
2019-12-06 21:28:00 -0300

    Correcao Comunicacao Api

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//importar lib axios
+//importar lib axios (Permite realizar requisicoes para fora do projeto atraves de request)
 const axios = require('axios');
 
 class App {
@@ -6,7 +6,8 @@ class App {
         this.buttonCreate = document.getElementById("btn_create");
         this.title = document.getElementById("input_title");
         this.content = document.getElementById("input_content");
-        this.getScraps(this);
+        //Trazer os recados ao iniciar a aplicacao
+        this.getScraps(this); //O THIS passa a aplicação correta
         this.registerEvents();
     }
 
@@ -14,16 +15,19 @@ class App {
         this.buttonCreate.onclick = (event) => this.createCard(event);
     }
 
-    //pegar os cards (GET)
+    // Funcao p/ pegar os RECADOS/CARDS (GET). APP é o nome criado p/ o THIS do construtor
     getScraps(app){
-        //fazer as chamadas do POSTMAN
+        //Pegar a constante e fazer as chamadas do POSTMAN (API Node)
         axios.get('http://localhost:3333/cards')
         .then(function(response){
             // console.log(app);
+
+            //Passando o DATA p/ a funcao recoveryScraps
             app.recoveryScraps(response.data);
+
             // this.recoveryScraps(response.data);
-            // console.log(response.data)
-            // console.log(response);
+            // console.log(response.data) //Retorna soh o DATA do objeto
+            // console.log(response); //Rerorna todo o objeto
         })
         .catch(function(error){
             console.log(error);
@@ -33,11 +37,26 @@ class App {
         });
     }
 
-    //quando executar o THEN do getScraps, vai passar a DATA pro recoveryScraps
+    //quando executar o THEN do getScraps, vai passar o DATA pro recoveryScraps
     recoveryScraps(data){
-        console.log(data);
-        for(item of data){
+        // //Retorna o array de objetos (cards)
+        // console.log(data);
+        //Percorre o array de objetos (cards)
+        for(item of data){ //dentro de ITEM ha as propriedades ID, TITLE e CONTENT
+            
+            //Reaproveitar as funcoes que possuem as mesmas propriedades de ITEM
+            const retornaHtml = this.cardLayout(item.title, item.content)
 
+            //Testando se o html esta sendo retornado
+            console.log(retornaHtml);
+
+            //Inserir o HTML no front
+            this.insertHtml(retornaHtml);
+
+            //Inserir tambem o evendo do botao de DELETAR do card
+            document.querySelectorAll('.delete-card').forEach(item => {
+                item.onclick = event => this.deleteCard(event);
+            });
         }
     }
 
